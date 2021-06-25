7cb5274df76d777586b0038f7680e31c58e0140b
Kang-Jh <iamkangjh@gmail.com>
2021-06-25 15:26:25 +0900

    유저가 아이템 선택 후 컨트롤 누르면 아이템 움직일 수 있게 이벤트 추가

--- src/components/MindMap.tsx
+++ src/components/MindMap.tsx
@@ -1,8 +1,36 @@
+import { useRecoilState } from 'recoil';
+import { itemAtom, singleSelectedIdAtom } from '../atoms';
+import styles from '../styles/MindMap.module.css';
 import Item from './Item';
 
 export default function MindMap({ items }: { items: number[] }) {
+  const [singleSelectedId, setSingleSelectedId] =
+    useRecoilState(singleSelectedIdAtom);
+  const [singleSelectedItem, setSingleSelectedItem] = useRecoilState(
+    itemAtom(singleSelectedId)
+  );
+
   return (
-    <div>
+    <div
+      className={styles.MindMapDiv}
+      onClick={() => {
+        setSingleSelectedItem((state) => ({
+          ...state,
+          selected: false,
+        }));
+        setSingleSelectedId(0);
+      }}
+      onMouseMove={(e) => {
+        e.preventDefault();
+        if (e.ctrlKey && singleSelectedItem.selected) {
+          setSingleSelectedItem((state) => ({
+            ...state,
+            top: state.top + e.movementY,
+            left: state.left + e.movementX,
+          }));
+        }
+      }}
+    >
       {items.map((id) => (
         <Item key={id} id={id} />
       ))}
