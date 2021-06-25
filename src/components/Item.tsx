04af3c544c8904876aceea28fe0777d791d78ed2
Kang-Jh <iamkangjh@gmail.com>
2021-06-25 15:26:03 +0900

    아이템 컴포넌트에 이벤트들 추가

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,20 +1,81 @@
 import styles from '../styles/Item.module.css';
-import { useRecoilState } from 'recoil';
-import { itemAtom } from '../atoms';
+import { useRecoilState, useSetRecoilState } from 'recoil';
+import { itemAtom, singleSelectedIdAtom } from '../atoms';
+import { useState } from 'react';
 
 export default function Item({ id }: { id: number }) {
   const [item, setItem] = useRecoilState(itemAtom(id));
+  const [isEdittingText, setIsEdittingText] = useState(false);
+  const setSingleSelectedId = useSetRecoilState(singleSelectedIdAtom);
 
   return (
     <div
-      className={styles.item}
+      className={`${styles.item} ${item.selected ? styles.selected : ''}`}
       style={{
         width: `${item.width}px`,
         height: `${item.height}px`,
         backgroundColor: item.bgColor,
+        top: item.top,
+        left: item.left,
+        zIndex: item.selected ? 100000 : 0,
+      }}
+      onClick={(e) => {
+        e.stopPropagation();
+
+        setItem((state) => ({
+          ...state,
+          selected: isEdittingText ? true : !state.selected,
+        }));
+        setSingleSelectedId(isEdittingText ? id : item.selected ? 0 : id);
+
+        setIsEdittingText(false);
       }}
     >
-      <div>{item.contents}</div>
+      {isEdittingText ? (
+        <input
+          className={styles.input}
+          type="text"
+          value={item.text}
+          onChange={(e) =>
+            setItem((state) => ({ ...state, text: e.target.value }))
+          }
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              setIsEdittingText(false);
+            }
+          }}
+          // 입력 필드 클릭시 아이템이 선택되는 것을 막기 위한 onClick
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          // 텍스트 드래그를 통한 선택을 위한 onMouseMove
+          onMouseMove={(e) => {
+            e.stopPropagation();
+          }}
+          autoFocus
+        />
+      ) : (
+        <span
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          onDoubleClick={(e) => {
+            e.stopPropagation();
+            if (e.ctrlKey || e.altKey) {
+              return;
+            }
+
+            setIsEdittingText(true);
+            setItem((state) => ({
+              ...state,
+              selected: true,
+            }));
+            setSingleSelectedId(id);
+          }}
+        >
+          {item.text}
+        </span>
+      )}
     </div>
   );
 }
