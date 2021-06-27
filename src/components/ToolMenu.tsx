b96f9d08a54003466914e7cb497fbecaa069b029
Kang-Jh <iamkangjh@gmail.com>
2021-06-27 10:46:18 +0900

    첫 아이템과 마지막 아이템 사용할 수 있도록 변경 및 마지막 아이템 내용 수정 기능 추가

--- src/components/ToolMenu.tsx
+++ src/components/ToolMenu.tsx
@@ -1,73 +1,141 @@
-import { useRecoilState, useSetRecoilState } from 'recoil';
-import { selectedItemsState, linkedItemsState } from '../atoms';
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
+import {
+  selectedItemsState,
+  linkedItemsState,
+  itemStateWithId,
+} from '../atoms';
 
 export default function ToolMenu() {
-  const [selectedItems, setSelectedItems] = useRecoilState(selectedItemsState);
+  const selectedItems = useRecoilValue(selectedItemsState);
   const setLinkedItems = useSetRecoilState(linkedItemsState);
+  const [firstSelectedItem, setFirstSelectedItem] = useRecoilState(
+    itemStateWithId(selectedItems[0]?.id ?? 0)
+  );
+  const [lastSelectedItem, setLastSelectedItem] = useRecoilState(
+    itemStateWithId(selectedItems[selectedItems.length - 1]?.id ?? 0)
+  );
 
   return (
     <div>
-      <div>
-        마지막으로 선택된 아이템
-        {/* 부등호를 써준 이유는 selectedItems.length 로 할 경우 길이가 0일 때 화면에 0이 표시되기 때문임 */}
-        {selectedItems.length > 0 &&
-          selectedItems[selectedItems.length - 1].text}
-      </div>
-
-      {selectedItems.length === 2 && (
+      {/* 부등호를 써준 이유는 selectedItems.length 로 할 경우 길이가 0일 때 화면에 0이 표시되기 때문임 */}
+      {lastSelectedItem.id > 0 && (
         <div>
-          <div>상위개념 {selectedItems[0].text}</div>
-          <div>하위개념 {selectedItems[1].text}</div>
+          <div>
+            <label htmlFor="itemText">개념</label>
+            <input
+              id="itemText"
+              type="text"
+              value={lastSelectedItem.text}
+              onChange={(e) =>
+                setLastSelectedItem((state) => ({
+                  ...state,
+                  text: e.target.value,
+                }))
+              }
+            />
+          </div>
+
+          <div>
+            <label htmlFor="itemRadius">반지름</label>
+            <input
+              id="itemRadius"
+              type="number"
+              value={lastSelectedItem.radius}
+              onChange={(e) =>
+                setLastSelectedItem((state) => ({
+                  ...state,
+                  radius: parseInt(e.target.value),
+                }))
+              }
+            />
+          </div>
+
+          <div>
+            <label htmlFor="positionX">X 좌표</label>
+            <input
+              id="positionX"
+              type="number"
+              value={lastSelectedItem.left}
+              onChange={(e) =>
+                setLastSelectedItem((state) => ({
+                  ...state,
+                  left: parseInt(e.target.value),
+                }))
+              }
+            />
+          </div>
+
+          <div>
+            <label htmlFor="positionY">Y 좌표</label>
+            <input
+              id="positionY"
+              type="number"
+              value={lastSelectedItem.top}
+              onChange={(e) =>
+                setLastSelectedItem((state) => ({
+                  ...state,
+                  top: parseInt(e.target.value),
+                }))
+              }
+            />
+          </div>
+        </div>
+      )}
+
+      {selectedItems.length >= 2 && (
+        <div>
+          <div>상위 개념 {firstSelectedItem.text}</div>
+          <div>하위 개념 {lastSelectedItem.text}</div>
           <button
             onClick={() => {
               if (
-                selectedItems[1].parent === null &&
-                !selectedItems[1].children.includes(selectedItems[0].id)
+                lastSelectedItem.parent === null &&
+                !lastSelectedItem.children.includes(firstSelectedItem.id)
               ) {
-                setSelectedItems(
-                  selectedItems.map((item, index) => ({
-                    ...item,
-                    parent: index === 0 ? item.parent : selectedItems[0].id,
-                    children:
-                      index === 0
-                        ? [...item.children, selectedItems[1].id]
-                        : item.children,
-                  }))
-                );
+                setFirstSelectedItem((state) => ({
+                  ...state,
+                  children: [...state.children, lastSelectedItem.id],
+                }));
+
+                setLastSelectedItem((state) => ({
+                  ...state,
+                  parent: firstSelectedItem.id,
+                }));
 
                 setLinkedItems((state) => [
                   ...state,
-                  JSON.stringify([selectedItems[0].id, selectedItems[1].id]),
+                  JSON.stringify([firstSelectedItem.id, lastSelectedItem.id]),
                 ]);
 
                 console.log('연결됨');
               } else if (
-                selectedItems[0].children.includes(selectedItems[1].id)
+                firstSelectedItem.children.includes(lastSelectedItem.id)
               ) {
-                setSelectedItems(
-                  selectedItems.map((item, index) => ({
-                    ...item,
-                    parent: index === 0 ? item.parent : null,
-                    children:
-                      index === 0
-                        ? item.children.filter(
-                            (id) => id !== selectedItems[1].id
-                          )
-                        : item.children,
-                  }))
-                );
+                setFirstSelectedItem((state) => ({
+                  ...state,
+                  children: state.children.filter(
+                    (id) => id !== lastSelectedItem.id
+                  ),
+                }));
+                setLastSelectedItem((state) => ({
+                  ...state,
+                  parent: null,
+                }));
 
                 setLinkedItems((state) =>
                   state.filter(
                     (pair) =>
                       pair !==
-                      JSON.stringify([selectedItems[0].id, selectedItems[1].id])
+                      JSON.stringify([
+                        firstSelectedItem.id,
+                        lastSelectedItem.id,
+                      ])
                   )
                 );
               }
             }}
           >
-            {selectedItems[0].children.includes(selectedItems[1].id)
+            {firstSelectedItem.children.includes(lastSelectedItem.id)
               ? '연결 해제'
               : '연결하기'}
           </button>
