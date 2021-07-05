45f92b92f0f4a44932db784315510b0b9515bfb3
Kang-Jh <iamkangjh@gmail.com>
2021-07-05 11:36:43 +0900

    주석 추가 및 stopPropagation 삭제

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,6 +1,12 @@
 import styles from '../styles/ColorPicker.module.css';
 import { useRef, useState } from 'react';
 
+/**
+ * setColor는 color를 인자로 받아서 어떤 식으로 상태를 업데이트 할 것인지를 나타내는 함수입니다
+ * @param {string} label 컬러 피커의 대상을 명시하는 문자열
+ * @param {string} color hsl() 형태인 문자열
+ * @param {(color: string) => void} setColor color를 인자로 받아 내부적으로 상태 업데이트를 실행하는 함수
+ */
 export default function ColorPicker({
   label,
   color,
@@ -10,11 +16,17 @@ export default function ColorPicker({
   color: string;
   setColor: (color: string) => void;
 }) {
+  // 모달의 위치를 정하기 위한 ref
   const colorPickerRef = useRef<HTMLDivElement>(null);
+  // 색상을 선택하는 직선 모양의 컬러 피커를 마우스로 눌렀는지 여부
   const [isHueMouseDown, setIsHueMouseDown] = useState(false);
+  // 채도와 밝기를 선택하는 직사각형 모양의 컬러 피커를 마우스로 눌렀는지 여부
   const [isSLSelectorMouseDown, setIsSelectorMouseDown] = useState(false);
+  // 이전 컬러로 되돌리기 위한 상태
   const [lastColor, setLastColor] = useState(color);
+  // 컬러 피커 모달이 열렸는지를 저장하는 상태
   const [isOpened, setIsOpened] = useState(false);
+  // 주어진 color에서 색상(Hue)를 나타내는 값
   const h = color.split(',')[0].slice(4).trim();
 
   return (
@@ -48,7 +60,6 @@ export default function ColorPicker({
                 background: `hsl(${h}, 100%, 50%)`,
               }}
               onMouseDown={(e) => {
-                e.stopPropagation();
                 setIsSelectorMouseDown(true);
                 if (e.target instanceof HTMLDivElement) {
                   // 타겟 직사각형
@@ -92,7 +103,6 @@ export default function ColorPicker({
                 }
               }}
               onMouseMove={(e) => {
-                e.stopPropagation();
                 if (
                   isSLSelectorMouseDown &&
                   e.target instanceof HTMLDivElement
@@ -123,11 +133,9 @@ export default function ColorPicker({
                 }
               }}
               onMouseUp={(e) => {
-                e.stopPropagation();
                 setIsSelectorMouseDown(false);
               }}
               onMouseLeave={(e) => {
-                e.stopPropagation();
                 setIsSelectorMouseDown(false);
               }}
             >
