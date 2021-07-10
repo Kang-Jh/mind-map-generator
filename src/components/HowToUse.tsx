50ecee641e1f2fcc39d9013b5dcd154f98ada852
Kang-Jh <iamkangjh@gmail.com>
2021-07-10 13:21:24 +0900

    HowToUse 컴포넌트 구현 및 사용

---
+++ src/components/HowToUse.tsx
@@ -0,0 +1,66 @@
+import styles from '../styles/HowToUse.module.css';
+import { Dispatch, SetStateAction } from 'react';
+
+export default function HowToUse({
+  isOpened,
+  setIsOpened,
+}: {
+  isOpened: boolean;
+  setIsOpened: Dispatch<SetStateAction<boolean>>;
+}) {
+  if (!isOpened) {
+    return null;
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.howToUse}>
+        <h2>마인드 맵 생성기 사용 방법</h2>
+        <ul className={styles.descriptionList}>
+          <li>
+            <p>
+              아이템을 선택하려면 왼쪽 아이템 목록에 있는 아이템을 클릭하시거나
+              마인드맵에 있는 아이템을 클릭하시면 됩니다.
+            </p>
+          </li>
+
+          <li>
+            <p>
+              아이템들을 선택한 후 왼쪽 Ctrl 키를 누른 채로 마우스를 움직이면
+              선택된 모든 아이템들이 움직입니다.
+            </p>
+          </li>
+
+          <li>
+            <p>
+              마인드맵 배경을 클릭하시면 선택된 모든 아이템들이 선택 해제됩니다.
+            </p>
+          </li>
+
+          <li>
+            <p>아이템 삭제는 선택된 모든 아이템들을 삭제합니다.</p>
+          </li>
+
+          <li>
+            <p>
+              2개의 아이템이 선택되었을 때 연결하기 버튼이 나타납니다. 단,
+              연결하기 버튼을 클릭하더라도 아이템들이 연결될 수 없는 경우
+              연결되지 않습니다.
+            </p>
+          </li>
+
+          <li>
+            <p>
+              서로 직접적으로 연결된 2개의 아이템이 선택되었을 때 연결 해제
+              버튼이 나타납니다.
+            </p>
+          </li>
+        </ul>
+
+        <button className={styles.btn} onClick={() => setIsOpened(false)}>
+          닫기
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/ItemsList.tsx
+++ src/components/ItemsList.tsx
@@ -9,6 +9,7 @@ import {
   totalItemsState,
 } from '../atoms';
 import ListItem from './ListItem';
+import HowToUse from './HowToUse';
 
 export default function ItemsList() {
   const [nextId, setNextId] = useState(1);
@@ -17,119 +18,129 @@ export default function ItemsList() {
   const [selectedItems, setSelectedItems] = useRecoilState(selectedItemsState);
   const rootedIds = useRecoilValue(rootedIdsState);
   const setTotalItemsState = useSetRecoilState(totalItemsState);
+  const [isHowToUseOpened, setIsHowToUseOpened] = useState(false);
 
   return (
-    <div className={styles.itemsListContainer}>
-      <div className={styles.buttons}>
-        <button className={styles.howToUseBtn}>사용법</button>
-
-        <button
-          className={styles.addBtn}
-          onClick={() => {
-            setIds((state) => [...state, nextId]);
-            setNextId(nextId + 1);
-          }}
-        >
-          아이템 생성
-        </button>
-
-        <button
-          className={styles.deleteBtn}
-          onClick={() => {
-            for (let i = 0; i < selectedItems.length; i++) {
-              const id = selectedItems[i].id;
-
-              setLinkedIds((state) =>
-                state.filter((pair) => !JSON.parse(pair).includes(id))
-              );
-
-              setIds((state) => state.filter((el) => el !== id));
-
-              setTotalItemsState((state) =>
-                state.map((item) => ({
-                  ...item,
-                  // 부모가 삭제되는 경우면 null로 그렇지 않은 경우엔 현재 부모를 그대로 사용
-                  parent: item.parent === id ? null : item.parent,
-                  // 자식 목록에 삭제되는 id가 있으면 필터링
-                  // 자식 목록에 삭제되는 id가 없으면 현재 자식 목록 그대로 사용
-                  children: item.children.includes(id)
-                    ? item.children.filter((el) => el !== id)
-                    : item.children,
-                }))
-              );
-            }
-          }}
-        >
-          아이템 삭제
-        </button>
-
-        {/* 연결 버튼 */}
-        {selectedItems.length === 2 && (
+    <>
+      <div className={styles.itemsListContainer}>
+        <div className={styles.buttons}>
           <button
-            className={styles.linkBtn}
+            className={styles.howToUseBtn}
+            onClick={() => setIsHowToUseOpened(true)}
+          >
+            사용법
+          </button>
+
+          <button
+            className={styles.addBtn}
             onClick={() => {
-              const parent = selectedItems[0];
-              const child = selectedItems[1];
+              setIds((state) => [...state, nextId]);
+              setNextId(nextId + 1);
+            }}
+          >
+            아이템 생성
+          </button>
 
-              if (child.parent === parent.id) {
-                // 자식의 부모 아이디와 현재 선택된 부모 아이디가 같은 경우
-                // 연결 해제시키고 linkedIds에서 아이디 쌍을 삭제
-
-                setSelectedItems((state) =>
-                  state.map((item) => ({
-                    ...item,
-                    parent: item.id === child.id ? null : item.parent,
-                    children:
-                      item.id === parent.id
-                        ? item.children.filter((id) => id !== child.id)
-                        : item.children,
-                  }))
-                );
+          <button
+            className={styles.deleteBtn}
+            onClick={() => {
+              for (let i = 0; i < selectedItems.length; i++) {
+                const id = selectedItems[i].id;
 
                 setLinkedIds((state) =>
-                  state.filter(
-                    (pair) => pair !== JSON.stringify([parent.id, child.id])
-                  )
+                  state.filter((pair) => !JSON.parse(pair).includes(id))
                 );
-              } else if (child.parent === null) {
-                // 자식의 부모가 아직 설정되지 않은 경우
-                // 부모와 자식을 연결 시키고 linkedIds에 아이디 쌍을 추가
-                setSelectedItems((state) =>
+
+                setIds((state) => state.filter((el) => el !== id));
+
+                setTotalItemsState((state) =>
                   state.map((item) => ({
                     ...item,
-                    parent: item.id === parent.id ? item.parent : parent.id,
-                    children:
-                      item.id === parent.id
-                        ? [...item.children, child.id]
-                        : item.children,
+                    // 부모가 삭제되는 경우면 null로 그렇지 않은 경우엔 현재 부모를 그대로 사용
+                    parent: item.parent === id ? null : item.parent,
+                    // 자식 목록에 삭제되는 id가 있으면 필터링
+                    // 자식 목록에 삭제되는 id가 없으면 현재 자식 목록 그대로 사용
+                    children: item.children.includes(id)
+                      ? item.children.filter((el) => el !== id)
+                      : item.children,
                   }))
                 );
-
-                setLinkedIds((state) => {
-                  const pair = JSON.stringify([parent.id, child.id]);
-                  if (state.includes(pair)) {
-                    return state;
-                  }
-
-                  return [...state, pair];
-                });
               }
             }}
           >
-            {selectedItems[1].parent === selectedItems[0].id
-              ? '연결 해제'
-              : '연결하기'}
+            아이템 삭제
           </button>
-        )}
+
+          {/* 연결 버튼 */}
+          {selectedItems.length === 2 && (
+            <button
+              className={styles.linkBtn}
+              onClick={() => {
+                const parent = selectedItems[0];
+                const child = selectedItems[1];
+
+                if (child.parent === parent.id) {
+                  // 자식의 부모 아이디와 현재 선택된 부모 아이디가 같은 경우
+                  // 연결 해제시키고 linkedIds에서 아이디 쌍을 삭제
+
+                  setSelectedItems((state) =>
+                    state.map((item) => ({
+                      ...item,
+                      parent: item.id === child.id ? null : item.parent,
+                      children:
+                        item.id === parent.id
+                          ? item.children.filter((id) => id !== child.id)
+                          : item.children,
+                    }))
+                  );
+
+                  setLinkedIds((state) =>
+                    state.filter(
+                      (pair) => pair !== JSON.stringify([parent.id, child.id])
+                    )
+                  );
+                } else if (child.parent === null) {
+                  // 자식의 부모가 아직 설정되지 않은 경우
+                  // 부모와 자식을 연결 시키고 linkedIds에 아이디 쌍을 추가
+                  setSelectedItems((state) =>
+                    state.map((item) => ({
+                      ...item,
+                      parent: item.id === parent.id ? item.parent : parent.id,
+                      children:
+                        item.id === parent.id
+                          ? [...item.children, child.id]
+                          : item.children,
+                    }))
+                  );
+
+                  setLinkedIds((state) => {
+                    const pair = JSON.stringify([parent.id, child.id]);
+                    if (state.includes(pair)) {
+                      return state;
+                    }
+
+                    return [...state, pair];
+                  });
+                }
+              }}
+            >
+              {selectedItems[1].parent === selectedItems[0].id
+                ? '연결 해제'
+                : '연결하기'}
+            </button>
+          )}
+        </div>
+
+        <ul className={styles.itemsList}>
+          {rootedIds.map((id) => (
+            <li key={id}>
+              <ListItem id={id} />
+            </li>
+          ))}
+        </ul>
       </div>
 
-      <ul className={styles.itemsList}>
-        {rootedIds.map((id) => (
-          <li key={id}>
-            <ListItem id={id} />
-          </li>
-        ))}
-      </ul>
-    </div>
+      <HowToUse isOpened={isHowToUseOpened} setIsOpened={setIsHowToUseOpened} />
+    </>
   );
 }
